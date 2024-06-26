89ce1a580b397baee2c832874fc8fbcc08449331
shaoh92 <huiling.shao1992@gmail.com>
2024-06-26 15:41:16 -0700

    new file:   OpenAlex-test.txt 	modified:   TS-migratory-insertion.py 	new file:   openalex-abstract-search.py 	new file:   pdf
    	deleted:    hello-world.py
    	deleted:    openalex-abstract-search.py
    	deleted:    pdf
    
    	CompDB-web.py
    	Download-from-OpenAlex.py
    	OAlex-download.py
    	OpenAlex-csv-clean-up.py
    	SI-download-from-doi.py
    	Web-Scrap-test.py
    	app.log
    	doi-to-SI-link.py
    	err/
    	not-needed-for-now/
    	works.csv

--- TS-migratory-insertion.py
+++ TS-migratory-insertion.py
@@ -33,63 +33,153 @@ for filename in os.listdir(directory_path):
             carbon_indices = [atom.GetIdx() for atom in mol.GetAtoms() if atom.GetSymbol() == 'C']  # Get indices of carbon atoms
 
             conf = mol.GetConformer()
-            is_hydride_migration_TS = False
-            condition0_C_H = False
-            condition1_M_H = False
-            condition2_M_C = False
-            condition3_C_M_H_angle = False
+#Successfully improted the .mol file and got the indices of transition metal, hydrogen and carbon atoms.
+                
+        #Getting the index of transition metal in this molecule and save it in TM_index. 
+        TM_index = None
+        for atom in mol.GetAtoms():
+            if is_transition_metal(atom):
+                atom_index = atom.GetIdx()
+                atom_type = atom.GetSymbol()
+                #print(f"Atom index: {atom_index}, Atom type: {atom_type}") # Debug print
+        
+                if TM_index is None:
+                    TM_index = atom_index  
+                    #print(f"TM index: {TM_index}") # Debug print
 
-        for carbon_index in carbon_indices:
-            for h_index in hydrogen_indices:
-                pos1 = conf.GetAtomPosition(carbon_index)
-                pos2 = conf.GetAtomPosition(h_index)
-                distance = pos1.Distance(pos2)
-                if 1.10 <= distance <= 1.50:
-                    condition0_C_H = True
-                    print(f"Condition 0 met with carbon index {carbon_index} and H index {h_index}")
-                    break
+        #Getting the index of the metal hydride in this molecule 
+        M_hydride_index = None
+        for h_index in hydrogen_indices:
+            pos1 = conf.GetAtomPosition(TM_index)
+            pos2 = conf.GetAtomPosition(h_index)
+            distance = pos1.Distance(pos2)
+            if 1.55 <= distance <= 1.75:
+                saved_h_index = h_index  # Save the hydrogen index
+                #print(f"M-H_distance: {distance}") # Debug print
+                #print(f"h index: {h_index}") # Debug print
+            # Get the bond between the hydrogen atom and the transition metal atom
+                if M_hydride_index is None:
+                    M_hydride_index = saved_h_index
+                    #print(f"M_hydride index: {M_hydride_index}") # Debug print 
 
-        for tm_index in transition_metal_indices:
-            for h_index in hydrogen_indices:
-                pos1 = conf.GetAtomPosition(tm_index)
-                pos2 = conf.GetAtomPosition(h_index)
-                distance = pos1.Distance(pos2)
-                if 1.55 <= distance <= 1.75:
-                    condition1_M_H = True
-                    saved_h_index = h_index  # Save the hydrogen index
-                    saved_tm_index = tm_index  # Save the transition metal index
-                    print(f"Condition 1 met with TM index {tm_index} and H index {h_index}")
-                    break
-            if condition1_M_H:  # Check condition1_M_H here
-                for carbon_index in carbon_indices:
-                    pos1 = conf.GetAtomPosition(tm_index)
-                    pos2 = conf.GetAtomPosition(carbon_index)
-                    distance = pos1.Distance(pos2)
-                    if 2.10 <= distance <= 2.25:
-                        condition2_M_C = True
-                        saved_carbon_index = carbon_index  # Save the carbon index
-                        print(f"Condition 2 met with TM index {tm_index} and C index {carbon_index}")  # Debug print
+        # Define a list of transition metals
+        transition_metals = ['Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+        
+        # Getting the index of the M_carbon in this molecule 
+        M_carbon_index = None
+        for atom in mol.GetAtoms():
+            if atom.GetSymbol() == 'C':
+                # Check if the carbon atom is bonded to a transition metal
+                for neighbor in atom.GetNeighbors():
+                    if neighbor.GetSymbol() in transition_metals:
+                        M_carbon_index = atom.GetIdx()
                         break
-            if condition0_C_H and condition1_M_H and condition2_M_C:  # Check both conditions here
-                tm_index = saved_tm_index
-                h_index = saved_h_index
-                carbon_index = saved_carbon_index
-                # Calculate the angle in radians
-                angle_rad = rdMolTransforms.GetAngleRad(conf, carbon_index, tm_index, h_index)
-                # Convert to degrees
-                angle_deg = math.degrees(angle_rad)  
-                if 70 <= angle_deg <= 100:
-                    condition3_C_M_H_angle = True
-                    print(f"Condition 3 met with angle {angle_deg}")  # Debug print
-                    break  # Break out of the loop over transition_metal_indices
+        
+                # If M_carbon_index is found, break the loop
+                if M_carbon_index is not None:
+                    break
+        #print(f"M_carbon index: {M_carbon_index}") # Debug print
 
-        if condition0_C_H and condition1_M_H and condition2_M_C and condition3_C_M_H_angle:
+        # Getting the index of the H_carbon in this molecule 
+        H_carbon_index = None
+        for atom in mol.GetAtoms():
+            if atom.GetSymbol() == 'C':
+                # Check if the carbon atom is bonded to a hydrogen atom and M_carbon
+                for neighbor in atom.GetNeighbors():
+                    if neighbor.GetSymbol() == 'H':
+                        bond_length = rdMolTransforms.GetBondLength(conf, atom.GetIdx(), neighbor.GetIdx())
+                        if 1.10 <= bond_length <= 1.80:
+                            for second_neighbor in atom.GetNeighbors():
+                                if second_neighbor.GetIdx() == M_carbon_index:
+                                    H_carbon_index = atom.GetIdx()
+                                    break
+
+                # If H_carbon_index is found, break the loop
+                if H_carbon_index is not None:
+                    break
+
+        # Print the H_carbon_index
+        #print(f"H_carbon index: {H_carbon_index}")
+                    
+        # Check if all indices are not None
+        if TM_index is None or M_hydride_index is None or H_carbon_index is None or M_carbon_index is None:
+            print("One or more indices are None. Skipping this molecule.")
+        else:
+            # Your conditions go here
+            print(f"TM index: {TM_index}, M_hydride index: {M_hydride_index}, H_carbon index: {H_carbon_index}, M_carbon index: {M_carbon_index}")            
+#Successfully obtained the indices of the four atoms involved in the hydride migration TS. Tested with TS2.mol file.
+
+        is_hydride_migration_TS = False
+        condition1_M_C_C = False
+        condition2_C_C_H = False
+        condition3_C_H_M = False
+        condition4_H_M_C = False
+        angle_rad = 0
+        angle_deg = 0
+
+        # Check condition 1: is the M_C_C bond angle between 60 and 120 degrees?
+        if TM_index is None or M_hydride_index is None or H_carbon_index is None or M_carbon_index is None:
+            print("One or more indices are None. Skipping this molecule.")
+        else:
+            angle_rad = rdMolTransforms.GetAngleRad(conf, TM_index, M_carbon_index, H_carbon_index)
+            #print(f"Angle: {angle_rad}") # Debug print
+        # Convert to degrees
+            angle_deg = math.degrees(angle_rad)  
+        #print(f"Angle in degrees: {angle_deg}") # Debug print
+            if 60 <= angle_deg <= 120:
+                condition1_M_C_C = True
+                #print(f"Condition 1 met with angle {angle_deg}")  # Debug print
+
+        # Check condition 2: is the C_C_H bond angle between 60 and 120 degrees?
+        if TM_index is None or M_hydride_index is None or H_carbon_index is None or M_carbon_index is None:
+            print("One or more indices are None. Skipping this molecule.")
+        else:
+            angle_rad = rdMolTransforms.GetAngleRad(conf, M_carbon_index, H_carbon_index, M_hydride_index)
+            #print(f"Angle: {angle_rad}") # Debug print
+        # Convert to degrees
+            angle_deg = math.degrees(angle_rad)  
+        #print(f"Angle in degrees: {angle_deg}") # Debug print
+            if 60 <= angle_deg <= 120:
+                condition2_C_C_H = True
+                #print(f"Condition 2 met with angle {angle_deg}")  # Debug print
+
+        # Check condition 3: is the C_H_M bond angle between 60 and 120 degrees?
+        if TM_index is None or M_hydride_index is None or H_carbon_index is None or M_carbon_index is None:
+            print("One or more indices are None. Skipping this molecule.")
+        else:        
+            angle_rad = rdMolTransforms.GetAngleRad(conf, H_carbon_index, M_hydride_index, TM_index)
+            angle_deg = math.degrees(angle_rad)  
+            #print(f"Angle in degrees: {angle_deg}") # Debug print
+            if 60 <= angle_deg <= 120:
+                condition3_C_H_M = True
+                #print(f"Condition 3 met with angle {angle_deg}")  # Debug print
+
+        # Check condition 4: is the H_M-C bond angle between 60 and 120 degrees?
+        if TM_index is None or M_hydride_index is None or H_carbon_index is None or M_carbon_index is None:
+            print("One or more indices are None. Skipping this molecule.")
+        else:
+            angle_rad = rdMolTransforms.GetAngleRad(conf, M_hydride_index, TM_index, M_carbon_index)
+            angle_deg = math.degrees(angle_rad)  
+            #print(f"Angle in degrees: {angle_deg}") # Debug print
+        if 60 <= angle_deg <= 120 or angle_deg == None:
+            condition4_H_M_C = True
+            #print(f"Condition 4 met with angle {angle_deg}")  # Debug print
+
+        # Check if all conditions are met
+        if condition1_M_C_C and condition2_C_C_H and condition3_C_H_M and condition4_H_M_C:
             is_hydride_migration_TS = True
-            print(f"The molecule {filename} is a hydride-migration-TS.")  # This should be printed if everything is correct
-            # Copy the .mol file to the new directory
-            try:
-                shutil.copy(file_path, new_directory_path)
-                #print(f"Successfully copied {filename} to {new_directory_path}.")
-            except Exception as e:
-                print(f"Failed to copy {filename} to {new_directory_path}. Error: {e}")
+        else:
+            is_hydride_migration_TS = False
+
+        print(f"Is {filename} a hydride migration TS? {is_hydride_migration_TS}")
+
+        # if condition0_C_H and condition1_M_H and condition2_M_C and condition3_C_M_H_angle:
+        #     is_hydride_migration_TS = True
+        #     print(f"The molecule {filename} is a hydride-migration-TS.")  # This should be printed if everything is correct
+        #     # Copy the .mol file to the new directory
+        #     try:
+        #         shutil.copy(file_path, new_directory_path)
+        #         #print(f"Successfully copied {filename} to {new_directory_path}.")
+        #     except Exception as e:
+        #         print(f"Failed to copy {filename} to {new_directory_path}. Error: {e}")
 
